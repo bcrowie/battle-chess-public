9fed1dc213bfcf6e3ff4065d0e86547cb0a27884
Brenden Crowie <brendencrowie@gmail.com>
2020-01-24 09:14:37 -0500

    Added game functionality, corrected issues
    Removed all direct assignments of private properties. All props have their own getter/setter
    
    Cleaned up code for loading save data functionality, removed nested loops where not absolutely necessary.
    
    Added check and checkmate detection. Each time a King is moved, the move is confirmed that it will not put the king in check. Both Kings are checked for Checkmate on each turn. If checkmate, winner is named and game ends.
    
    Pieces can no longer move or attack if their path is obstructed. This works with Check and Checkmate detection.
    
    Few other changes.

---
+++ src/objects/pieces/Queen.js
@@ -0,0 +1,49 @@
+const Piece = require("../Piece");
+const { COLORS } = require("../../constants");
+const chalk = require("chalk");
+
+class Queen extends Piece {
+  constructor(location, color) {
+    super(20, 5, location, color);
+    this._unicode = this._setUnicode();
+  }
+
+  _setUnicode() {
+    return this.getColor() === COLORS.WHITE
+      ? chalk.white(`\u2655`)
+      : chalk.black(`\u265B`);
+  }
+
+  _canMove(loc) {
+    const current = this.getLocation();
+    const x = loc[1];
+    const y = loc[0];
+    let result = false;
+    for (let i = 1; i < 9; i++) {
+      if (
+        (x === current[1] + i && y === current[0] + i) ||
+        (x === current[1] - i && y === current[0] - i) ||
+        (x === current[1] - i && y === current[0] + i) ||
+        (x === current[1] + i && y === current[0] - i)
+      ) {
+        result = true;
+      }
+    }
+    if (
+      ((y === current[0] && x > current[1]) ||
+        (x === current[1] && y > current[0]) ||
+        (y === current[0] && x < current[1]) ||
+        (x === current[1] && y < current[0])) &&
+      result === false
+    ) {
+      result = true;
+    }
+    return result;
+  }
+
+  _canAttack(to, from) {
+    return this._canMove(to.getLocation()) ? true : false;
+  }
+}
+
+module.exports = Queen;
