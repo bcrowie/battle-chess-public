80f7f3d2e15932f8187eb188de104a2bffbd406d
Brenden Crowie <brendencrowie@gmail.com>
2024-05-26 17:06:32 -0400

    Changed printBoard to use selected colors in Board

--- src/objects/Game.js
+++ src/objects/Game.js
@@ -64,43 +64,36 @@ class Game {
   // prettier-ignore
   printBoard() {
     console.clear() 
+    console.log(this.getBoard().getColors())
     const board = this.getBoard().getGameBoard();
-    const inv = chalk.bgGrey;
-
-    // function addBlankLines(){
-    //   let lines = process.stdout.getWindowSize()[1];
-    //   for(let i = 0; i < lines; i++) {
-    //     console.log('\r\n')
-    //   }
-    // }
-
-    // addBlankLines()
+    const { dark, light } = this.getBoard().getColors();
     
+
     console.log(`Battle-Chess
-                      ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     
-White          A   ${board.A1}  ${inv(`  ${board.A2}  `)}  ${board.A3}  ${inv(`  ${board.A4}  `)}  ${board.A5}  ${inv(`  ${board.A6}  `)}  ${board.A7}  ${inv(`  ${board.A8}  `)}          Black
-Health:               ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}        Health:
-King: ${this.getBoard().getPieces().WHITE.King.getHealth()}         ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}             King: ${this.getBoard().getPieces().BLACK.King.getHealth()}
-Queen: ${this.getBoard().getPieces().WHITE.Queen.getHealth()}      B ${inv(`  ${board.B1}  `)}  ${board.B2}  ${inv(`  ${board.B3}  `)}  ${board.B4}  ${inv(`  ${board.B5}  `)}  ${board.B6}  ${inv(`  ${board.B7}  `)}  ${board.B8}        Queen: ${this.getBoard().getPieces().BLACK.Queen.getHealth()}
-Bishop: ${this.getBoard().getPieces().WHITE.BishopOne.getHealth()}       ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}           Bishop: ${this.getBoard().getPieces().BLACK.BishopOne.getHealth()}
-Bishop: ${this.getBoard().getPieces().WHITE.BishopTwo.getHealth()}            ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}      Bishop: ${this.getBoard().getPieces().BLACK.BishopTwo.getHealth()}
-Knight: ${this.getBoard().getPieces().WHITE.KnightOne.getHealth()}     C   ${board.C1}  ${inv(`  ${board.C2}  `)}  ${board.C3}  ${inv(`  ${board.C4}  `)}  ${board.C5}  ${inv(`  ${board.C6}  `)}  ${board.C7}  ${inv(`  ${board.C8}  `)}     Knight: ${this.getBoard().getPieces().BLACK.KnightOne.getHealth()}
-Knight: ${this.getBoard().getPieces().WHITE.KnightTwo.getHealth()}            ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     Knight: ${this.getBoard().getPieces().BLACK.KnightTwo.getHealth()}
-Rook: ${this.getBoard().getPieces().WHITE.RookOne.getHealth()}         ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}            Rook: ${this.getBoard().getPieces().BLACK.RookOne.getHealth()}      
-Rook: ${this.getBoard().getPieces().WHITE.RookTwo.getHealth()}       D ${inv(`  ${board.D1}  `)}  ${board.D2}  ${inv(`  ${board.D3}  `)}  ${board.D4}  ${inv(`  ${board.D5}  `)}  ${board.D6}  ${inv(`  ${board.D7}  `)}  ${board.D8}         Rook: ${this.getBoard().getPieces().BLACK.RookTwo.getHealth()}
-                 ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     
-                      ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     
-               E   ${board.E1}  ${inv(`  ${board.E2}  `)}  ${board.E3}  ${inv(`  ${board.E4}  `)}  ${board.E5}  ${inv(`  ${board.E6}  `)}  ${board.E7}  ${inv(`  ${board.E8}  `)}               
-                      ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}
-                 ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     
-               F ${inv(`  ${board.F1}  `)}  ${board.F2}  ${inv(`  ${board.F3}  `)}  ${board.F4}  ${inv(`  ${board.F5}  `)}  ${board.F6}  ${inv(`  ${board.F7}  `)}  ${board.F8}
-                 ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}
-                      ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}
-               G   ${board.G1}  ${inv(`  ${board.G2}  `)}  ${board.G3}  ${inv(`  ${board.G4}  `)}  ${board.G5}  ${inv(`  ${board.G6}  `)}  ${board.G7}  ${inv(`  ${board.G8}  `)}               
-                      ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}
-                 ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}
-               H ${inv(`  ${board.H1}  `)}  ${board.H2}  ${inv(`  ${board.H3}  `)}  ${board.H4}  ${inv(`  ${board.H5}  `)}  ${board.H6}  ${inv(`  ${board.H7}  `)}  ${board.H8}        
-                 ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}     ${inv(`     `)}       
+                 ${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}
+White          A ${dark(`  ${board.A1}  `)}${light(`  ${board.A2}  `)}${dark(`  ${board.A3}  `)}${light(`  ${board.A4}  `)}${dark(`  ${board.A5}  `)}${light(`  ${board.A6}  `)}${dark(`  ${board.A7}  `)}${light(`  ${board.A8}  `)}          Black
+Health:          ${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}   Health:
+King: ${this.getBoard().getPieces().WHITE.King.getHealth()}         ${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}        King: ${this.getBoard().getPieces().BLACK.King.getHealth()}
+Queen: ${this.getBoard().getPieces().WHITE.Queen.getHealth()}      B ${light(`  ${board.B1}  `)}${dark(`  ${board.B2}  `)}${light(`  ${board.B3}  `)}${dark(`  ${board.B4}  `)}${light(`  ${board.B5}  `)}${dark(`  ${board.B6}  `)}${light(`  ${board.B7}  `)}${dark(`  ${board.B8}  `)}      Queen: ${this.getBoard().getPieces().BLACK.Queen.getHealth()}
+Bishop: ${this.getBoard().getPieces().WHITE.BishopOne.getHealth()}       ${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}      Bishop: ${this.getBoard().getPieces().BLACK.BishopOne.getHealth()}
+Bishop: ${this.getBoard().getPieces().WHITE.BishopTwo.getHealth()}       ${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}      Bishop: ${this.getBoard().getPieces().BLACK.BishopTwo.getHealth()}
+Knight: ${this.getBoard().getPieces().WHITE.KnightOne.getHealth()}     C ${dark(`  ${board.C1}  `)}${light(`  ${board.C2}  `)}${dark(`  ${board.C3}  `)}${light(`  ${board.C4}  `)}${dark(`  ${board.C5}  `)}${light(`  ${board.C6}  `)}${dark(`  ${board.C7}  `)}${light(`  ${board.C8}  `)}     Knight: ${this.getBoard().getPieces().BLACK.KnightOne.getHealth()}
+Knight: ${this.getBoard().getPieces().WHITE.KnightTwo.getHealth()}       ${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}     Knight: ${this.getBoard().getPieces().BLACK.KnightTwo.getHealth()}
+Rook: ${this.getBoard().getPieces().WHITE.RookOne.getHealth()}         ${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}       Rook: ${this.getBoard().getPieces().BLACK.RookOne.getHealth()}      
+Rook: ${this.getBoard().getPieces().WHITE.RookTwo.getHealth()}       D ${light(`  ${board.D1}  `)}${dark(`  ${board.D2}  `)}${light(`  ${board.D3}  `)}${dark(`  ${board.D4}  `)}${light(`  ${board.D5}  `)}${dark(`  ${board.D6}  `)}${light(`  ${board.D7}  `)}${dark(`  ${board.D8}  `)}       Rook: ${this.getBoard().getPieces().BLACK.RookTwo.getHealth()}
+                 ${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}
+                 ${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}     
+               E ${dark(`  ${board.E1}  `)}${light(`  ${board.E2}  `)}${dark(`  ${board.E3}  `)}${light(`  ${board.E4}  `)}${dark(`  ${board.E5}  `)}${light(`  ${board.E6}  `)}${dark(`  ${board.E7}  `)}${light(`  ${board.E8}  `)}               
+                 ${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}
+                 ${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}
+               F ${light(`  ${board.F1}  `)}${dark(`  ${board.F2}  `)}${light(`  ${board.F3}  `)}${dark(`  ${board.F4}  `)}${light(`  ${board.F5}  `)}${dark(`  ${board.F6}  `)}${light(`  ${board.F7}  `)}${dark(`  ${board.F8}  `)}
+                 ${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}
+                 ${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}
+               G ${dark(`  ${board.G1}  `)}${light(`  ${board.G2}  `)}${dark(`  ${board.G3}  `)}${light(`  ${board.G4}  `)}${dark(`  ${board.G5}  `)}${light(`  ${board.G6}  `)}${dark(`  ${board.G7}  `)}${light(`  ${board.G8}  `)}               
+                 ${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}
+                 ${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}
+               H ${light(`  ${board.H1}  `)}${dark(`  ${board.H2}  `)}${light(`  ${board.H3}  `)}${dark(`  ${board.H4}  `)}${light(`  ${board.H5}  `)}${dark(`  ${board.H6}  `)}${light(`  ${board.H7}  `)}${dark(`  ${board.H8}  `)}      
+                 ${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')}${light(`     `)}${dark('     ')} 
 Captures:          1    2    3    4    5    6    7    8         Captures:
 ${this._board.getCaptures(COLORS.WHITE).join("")}                                         ${this._board.getCaptures(COLORS.BLACK).join("")} 
 ${this.getBoard().getMessage()}`)
